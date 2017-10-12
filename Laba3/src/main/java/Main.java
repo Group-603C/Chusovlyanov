395260df1970c610984679522de5e388f2925528
i1ya97 <i1ya97@users.noreply.github.com>
2017-10-12 19:13:00 +0700

    Реализованно проверка входных данных

--- Laba3/src/main/java/Main.java
+++ Laba3/src/main/java/Main.java
@@ -1,10 +1,64 @@
 package main.java;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String expresion = "5*3+2*(12-4)";
-        Calculator input =new Calculator();
-        double rezult =input.Calculate(expresion);
-        System.out.println("Результат: "+rezult);
+        String expresion = InputArray();
+        Calculator input = new Calculator();
+        double rezult = input.Calculate(expresion);
+        System.out.println("Результат: " + rezult);
     }
+
+    public static boolean IsCorrect(String sequence) {
+        int summa = 0;
+        int number = 0;
+        for (int i = 0; i < sequence.length(); i++) {
+            if (sequence.charAt(i) == '(') {
+                summa++;
+                number++;
+            } else if ("0123456789-+=/* ".indexOf(sequence.charAt(i)) != -1) {
+                continue;
+            } else {
+                summa--;
+                number++;
+            }
+
+            if (summa < 0) {
+                return false;
+            }
+        }
+        if (number == sequence.length()) {
+            return false;
+        }
+
+        return summa == 0;
+    }
+
+    public static String InputArray() {
+        while (true) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Введите выражение");
+            String in = input.next();
+
+            int i = 0;
+            try {
+                while (i < in.length()) {
+                    if ("0123456789()-+=/* ".indexOf(in.charAt(i)) == -1) {
+                        throw new Error();
+                    }
+                    i++;
+                }
+                if (!IsCorrect(in)) {
+                    throw new Error();
+                }
+
+                return in;
+
+            } catch (Error e) {
+                System.out.println("Ошибка,введено не числовое значение!");
+            }
+        }
+    }
+
 }
--- Laba3/src/test/java/CalculatorTest.java
+++ Laba3/src/test/java/CalculatorTest.java
@@ -7,10 +7,10 @@ import static org.junit.Assert.*;
 public class CalculatorTest {
     @org.junit.Test
     public void calculate() throws Exception {
-        Calculator test1 =new Calculator();
+        Calculator test1 = new Calculator();
         double expected = 31d;
         double actual = test1.Calculate("5*3+2*(12-4)");
-        assertEquals("Test",expected,actual,0.001);
+        assertEquals("Test", expected, actual, 0.001);
     }
 
 }
