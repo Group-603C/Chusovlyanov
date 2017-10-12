f51a1a4e4d76a0d477ffedc6769c85c7ddbe513c
i1ya97 <i1ya97@users.noreply.github.com>
2017-10-12 16:10:33 +0700

    Теперь работает

--- Laba3/src/Main.java
+++ Laba3/src/Main.java
@@ -1,85 +1,97 @@
-import java.util.EmptyStackException;
+
 import java.util.Stack;
 
 public class Main {
     public static void main(String[] args){
-        String expresion = "(3+10)-2*(1-4)";
-        int a= Calculate (expresion);
+        String expresion = "(4*5)-(56-20)" ;
+        double a= Calculate (expresion);
         System.out.println(a);
     }
+    public static double Calculate  (String expresion){
+        String postfix = GetExpression(expresion);
+        double rezult =Counting(postfix);
+        return rezult;
+    }
 
-    public static int Priorities(char sign){
-            int prior;
-            switch (sign){
-                case '(':prior=1;break;
-                case ')':prior=2;break;
-                case '=':prior=3;break;
-                case '+':prior=4;break;
-                case '-':prior=4;break;
-                case '*':prior=5;break;
-                case '/':prior=5;break;
-                case ' ':prior=-1;break;
-                default:prior=0;
-            }
-            return prior;
+    private static boolean IsDelimeter(char ch) {
+        if ((" =".indexOf(ch) != -1))
+            return true;
+        return false;
     }
-    public static int Calculate (String expresion){
-        String postfix=PostfixTranslation(expresion);
-        Stack number= new Stack();
-        Stack znaki= new Stack();
+
+    private static boolean IsOperator(char сh) {
+        if (("+-/*".indexOf(сh) != -1))
+            return true;
+        return false;
+    }
+
+    private static int Priority (char ch){
+        switch (ch){
+            case '(':return 1;
+            case ')':return 2;
+            case '=':return 3;
+            case '+':return 4;
+            case '-':return 4;
+            case '*':return 5;
+            case '/':return 5;
+            case ' ':return -1;
+            default:return 0;
+        }
+    }
+
+    private static double Counting  (String postfix) {
+        double rezult = 0;
+        Stack temp = new Stack();
         char[] str = postfix.toCharArray();
-        for (int i=0;i<str .length;i++){
-            if((char)47<str[i]&&str[i]<(char)58){
-                char[] arr = new char[20];
-                int j=0;
-                while(str[i]!=' '){
-                    arr[j] =str[i];
-                    j++;
+        for (int i=0;i<str.length;i++) {
+            if (Character.isDigit(str[i])) {
+                String a = "";
+                while (!IsDelimeter(str[i]) && !IsOperator(str[i])) {
+                    a += str[i];
                     i++;
+                    if (i == str.length) {
+                        break;
+                    }
                 }
-                arr[j]='\0';
-                number.push(arr);
-            }
-            if(str[i]=='/'||str[i]=='*'||str[i]=='-'||str[i]=='+'){
-                znaki.push(str[i]);
-            }
-        }
-        int x,result=1;
-        char znak;
-        while(!number.empty()&&!znaki.empty()){
-            znak=(char)znaki.pop();
-            result=(int)number.pop();
-            x=(int)number.pop();
-            if(znak=='-'){
-                result=x-result;
-            }
-            if(znak=='+'){
-                result=x+result;
-            }
-            if(znak=='*'){
-                result=x*result;
-            }
-            if(znak=='/'){
-                result=x/result;
+                temp.push(Double.parseDouble(a));
+                i--;
+            } else if (IsOperator(str[i])) {
+                double a = (double) temp.pop();
+                double b = (double) temp.pop();
+                switch (str[i]) {
+                    case '+':
+                        rezult = b + a;break;
+                    case '-':
+                        rezult = b - a;break;
+                    case '*':
+                        rezult = b * a;break;
+                    case '/':
+                        rezult = b / a;break;
+                }
+                temp.push(rezult);
             }
         }
-        return result;
+        return (double) temp.peek();
     }
-    public static String PostfixTranslation (String expresion){
+
+    private static String GetExpression  (String expresion){
         int x;
         String postfix = "";
         Stack st= new Stack();
         char[] str = expresion.toCharArray();
         for(int i=0;i<str.length;i++) {
-            x = Priorities(str[i]);
+            x = Priority(str[i]);
             if(x==-1){
                 continue;
             }
             if (x == 0 || x == 3) {
-                while (x==0){
+                while (x==0&&i<str.length){
                     postfix += str[i];
                     i++;
-                    x = Priorities(str[i]);
+                    if(i==str.length){
+                        break;
+                    }
+                    x = Priority(str[i]);
                 }
                 postfix += " ";
                 i--;
@@ -98,7 +110,7 @@ public class Main {
                 continue;
             }
             if (x == 4 || x == 5) {
-                while (st.empty()==false && Priorities((char) st.peek()) >= x) {
+                while (st.empty()==false && Priority((char) st.peek()) >= x) {
                     postfix += (char) st.pop()+" ";
                 }
                 st.push(str[i]);
