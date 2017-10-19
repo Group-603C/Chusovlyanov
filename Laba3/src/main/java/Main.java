a3d0b23b1ecc0a9d9fdf41817e6ca374253e2a06
i1ya97 <31565819+i1ya97@users.noreply.github.com>
2017-10-19 22:30:56 +0700

    Переход на список

--- Laba3/src/main/java/Main.java
+++ Laba3/src/main/java/Main.java
@@ -4,56 +4,59 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String expresion = InputArray();
+        String expression = InputArray();
         Calculator input = new Calculator();
-        double rezult = input.Calculate(expresion);
-        System.out.println("Результат: " + rezult);
+        double result = Calculator.Calculate(expression);
+        System.out.println("Результат: " + result);
     }
 
     public static boolean IsCorrect(String sequence) {
-        int summa = 0;
-        int number = 0;
+        int sum = 0, number = 0;
+
         for (int i = 0; i < sequence.length(); i++) {
             if (sequence.charAt(i) == '(') {
-                summa++;
+                sum++;
                 number++;
-            } else if ("0123456789-+=/* ".indexOf(sequence.charAt(i)) != -1) {
+
+            } else if ("0123456789-+/*".indexOf(sequence.charAt(i)) != -1) {
                 continue;
+
             } else {
-                summa--;
+                sum--;
                 number++;
             }
- 
-            if (summa < 0) {
+
+            if (sum < 0) {
                 return false;
             }
+
         }
         if (number == sequence.length()) {
             return false;
         }
 
-        return summa == 0;
+        return sum == 0;
     }
 
     public static String InputArray() {
         while (true) {
-            Scanner input = new Scanner(System.in);
+            Scanner in = new Scanner(System.in);
             System.out.println("Введите выражение");
-            String in = input.next();
+            String input = in.next();
 
-            int i = 0;
             try {
-                while (i < in.length()) {
-                    if ("0123456789()-+=/* ".indexOf(in.charAt(i)) == -1) {
+                int count = 0;
+                while (count < input.length()) {
+                    if ("0123456789()-+/*".indexOf(input.charAt(count)) == -1) {
                         throw new Error();
                     }
-                    i++;
+                    count++;
                 }
-                if (!IsCorrect(in)) {
+                if (!IsCorrect(input)) {
                     throw new Error();
                 }
 
-                return in;
+                return input;
 
             } catch (Error e) {
                 System.out.println("Ошибка,введено не числовое значение!");
