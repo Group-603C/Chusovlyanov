fb0d10d823a2e59273eec86e023832c949fb7603
i1ya97 <31565819+i1ya97@users.noreply.github.com>
2017-10-20 20:35:31 +0700

    Недостаточно тестов

--- Laba3/src/test/java/CalculatorTest.java
+++ Laba3/src/test/java/CalculatorTest.java
@@ -6,11 +6,38 @@ import static org.junit.Assert.*;
 
 public class CalculatorTest {
     @org.junit.Test
-    public void calculate() throws Exception {
-        Calculator test1 = new Calculator();
-        double expected = 31d;
-        double actual = test1.Calculate("5*3+2*(12-4)");
+    public void TestAddition() throws Exception {
+        Calculator testAddition = new Calculator();
+        double expected = 34d;
+        double actual = testAddition.Calculate("11+23");
         assertEquals("Test", expected, actual, 0.001);
     }
-
+    @org.junit.Test
+    public void TestSubtraction() throws Exception{
+        Calculator testSubtraction = new Calculator();
+        double expected = 10d;
+        double actual=testSubtraction.Calculate("23-13");
+        assertEquals("Test", expected, actual, 0.001);
+    }
+    @org.junit.Test
+    public void TestSegmentation() throws Exception{
+        Calculator testSegmentation = new Calculator();
+        double expected = 9d;
+        double actual=testSegmentation.Calculate("45/5");
+        assertEquals("Test", expected, actual, 0.001);
+    }
+    @org.junit.Test
+    public void TestMultiplication() throws Exception{
+        Calculator testMultiplication = new Calculator();
+        double expected = 225d;
+        double actual=testMultiplication.Calculate("45*5");
+        assertEquals("Test", expected, actual, 0.001);
+    }
+    @org.junit.Test
+    public void TestBrackets() throws Exception{
+        Calculator testBrackets = new Calculator();
+        double expected = 25d;
+        double actual=testBrackets.Calculate("(45*5)-(100*2)");
+        assertEquals("Test", expected, actual, 0.001);
+    }
 }
