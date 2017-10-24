1a7123d5feb655f4fcd4ff202cc2140fb4dd596a
i1ya97 <31565819+i1ya97@users.noreply.github.com>
2017-10-24 13:14:47 +0600

    Update CalculatorTest.java

--- Laba3/src/test/java/CalculatorTest.java
+++ Laba3/src/test/java/CalculatorTest.java
@@ -7,37 +7,43 @@ import static org.junit.Assert.*;
 public class CalculatorTest {
     @org.junit.Test
     public void TestAddition() throws Exception {
-        Calculator testAddition = new Calculator();
-        double expected = 34d;
-        double actual = testAddition.Calculate("11+23");
-        assertEquals("Test", expected, actual, 0.001);
+
+        double actual = Calculator.Calculate("11+23");
+        assertEquals("Addition check", 34d, actual, 0.001);
     }
+
     @org.junit.Test
     public void TestSubtraction() throws Exception{
-        Calculator testSubtraction = new Calculator();
-        double expected = 10d;
-        double actual=testSubtraction.Calculate("23-13");
-        assertEquals("Test", expected, actual, 0.001);
+
+        double actual= Calculator.Calculate("23-13");
+        assertEquals("Check for subtraction", 10d, actual, 0.001);
     }
+
     @org.junit.Test
     public void TestSegmentation() throws Exception{
-        Calculator testSegmentation = new Calculator();
-        double expected = 9d;
-        double actual=testSegmentation.Calculate("45/5");
-        assertEquals("Test", expected, actual, 0.001);
+
+        double actual=Calculator.Calculate("45/5");
+        assertEquals("Fission check", 9d, actual, 0.001);
     }
+
     @org.junit.Test
     public void TestMultiplication() throws Exception{
-        Calculator testMultiplication = new Calculator();
-        double expected = 225d;
-        double actual=testMultiplication.Calculate("45*5");
-        assertEquals("Test", expected, actual, 0.001);
+
+        double actual=Calculator.Calculate("45*5");
+        assertEquals("Multiplication check", 225d, actual, 0.001);
     }
+
     @org.junit.Test
     public void TestBrackets() throws Exception{
-        Calculator testBrackets = new Calculator();
-        double expected = 25d;
-        double actual=testBrackets.Calculate("(45*5)-(100*2)");
-        assertEquals("Test", expected, actual, 0.001);
+
+        double actual=Calculator.Calculate("(45*1)*(102-2)");
+        assertEquals("Checking parentheses", 4500d, actual, 0.001);
+    }
+
+    @org.junit.Test
+    public void TestNull() throws Exception{
+
+        double actual=Calculator.Calculate(null);
+        assertEquals("Return null", 0, actual, 0.001);
     }
 }
