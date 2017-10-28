920b7d894e285430a6dae6e3ab4f52a8eabf2891
i1ya97 <i1ya97@users.noreply.github.com>
2017-10-28 19:39:45 +0700

    Добавлены Unit тесты

--- Laba3/src/main/java/Calculator.java
+++ Laba3/src/main/java/Calculator.java
@@ -1,142 +1,150 @@
 package main.java;
 
+import java.util.ArrayList;
 import java.util.Stack;
 
-
 public class Calculator {
+
     public static double Calculate(String expresion) {
-        String postfix = GetExpression(expresion);
+        ArrayList<String> postfix = GetExpression(expresion);
         double rezult = Counting(postfix);
         return rezult;
     }
 
-    private static boolean IsDelimeter(char ch) { //метод возвращает true,если проверяемый символ разделитель
-        if ((" =".indexOf(ch) != -1))
+    private static ArrayList<String> GetExpression(String expresion) {
+        ArrayList<String> arrayList = StringtoList(expresion);
+        ArrayList<String> result = new ArrayList<String>();
+        Stack stack = new Stack();
+        int priority;
+        for (String i : arrayList) {
+            priority = GetPriority(i);
+
+            if (priority == 0) {
+                result.add(i);
+            }
+
+            else if (priority == 1) {
+                stack.push(i);//добавляем к стек
+            }
+
+            else if (priority == 2) {
+                String symbol = (String) stack.pop();
+                while (GetPriority(symbol)!=1 && !stack.empty()) {
+                    result.add(symbol);
+                    symbol = (String) stack.pop();
+                }
+                continue;
+            }
+
+            else if (IsOperator(i)) {//если  +-/*
+                while (!stack.empty() && GetPriority((String) stack.peek()) >= priority) {
+                    result.add((String) stack.pop());//извлекаем из стека в строку
+                }
+                stack.push(i);//заносим новый оператор в строку
+            }
+        }
+
+        while (!stack.empty()) {//забираем последнии символы из стека
+            result.add((String) stack.pop());
+        }
+
+        return result;//возвращаем выражение в постфиксной записи
+    }
+
+    private static ArrayList StringtoList(String ch) {
+        ArrayList<String> result = new ArrayList();
+        char[] input = ch.toCharArray();
+        for (int i = 0; i < input.length; i++) {
+            if (" ".indexOf(input[i])==1){
+                continue;
+            } else if (Character.isDigit(input[i])) {
+                String b = "";
+                while (!IsDelimeter(String.valueOf(input[i])) && !IsOperator(String.valueOf(input[i]))) {//пока не разделитель
+                    b += input[i];//собираем число
+                    i++;
+                    if (i == input.length) {
+                        break;
+                    }
+                }
+                i--;
+                result.add(b);
+            } else {
+                result.add(String.valueOf(input[i]));
+            }
+        }
+        return result;
+    }
+
+    private static double Counting(ArrayList<String> postfix) {
+        Stack stack = new Stack();
+        double rezult = 0;
+
+        for (String i : postfix) {
+
+            if (sNumber(i)) { //если символ - цифра, то читаем все число и записываем на вершину стека
+                stack.push(Double.parseDouble((i)));//записываем в стек
+
+            } else if (IsOperator(i)) { //если символ -оператор
+                //берем числа из стека
+                double a = (double) stack.pop();
+                double b = (double) stack.pop();
+                //выполняем операции над ними
+                switch (i) {
+                    case "+":
+                        rezult = b + a;
+                        break;
+                    case "-":
+                        rezult = b - a;
+                        break;
+                    case "*":
+                        rezult = b * a;
+                        break;
+                    case "/":
+                        rezult = b / a;
+                        break;
+                }
+                stack.push(rezult);//результат возвращаем в стек
+            }
+        }
+        return (double) stack.peek();//возвращаем результат всех вычислений
+    }
+
+    private static boolean IsDelimeter(String ch) { //метод возвращает true,если проверяемый символ разделитель
+        if (("()".indexOf(ch) != -1))
             return true;
         return false;
     }
 
-    private static boolean IsOperator(char сh) { //метод возвращает true, если проверяемый символ - оператор
+    private static boolean IsOperator(String сh) { //метод возвращает true, если проверяемый символ - оператор
         if (("+-/*".indexOf(сh) != -1))
             return true;
         return false;
     }
 
-    private static int Priority(char ch) { //метод возврацения приоритета
+    private static int GetPriority(String ch) { //метод возврацения приоритета
         switch (ch) {
-            case '(':
+            case "(":
                 return 1;
-            case ')':
+            case ")":
                 return 2;
-            case '=':
+            case "+":
                 return 3;
-            case '+':
+            case "-":
+                return 3;
+            case "*":
                 return 4;
-            case '-':
+            case "/":
                 return 4;
-            case '*':
-                return 5;
-            case '/':
-                return 5;
-            case ' ':
-                return -1;
             default:
                 return 0;
         }
     }
 
-    private static double Counting(String postfix) {
-        double rezult = 0;
-        Stack temp = new Stack();
-        char[] str = postfix.toCharArray();
-
-        for (int i = 0; i < str.length; i++) {
-
-            if (Character.isDigit(str[i])) { //если символ - цифра, то читаем все число и записываем на вершину стека
-                String a = "";
-
-                while (!IsDelimeter(str[i]) && !IsOperator(str[i])) {//пока не разделитель
-                    a += str[i];//собираем число
-                    i++;
-
-                    if (i == str.length) {
-                        break;
-                    }
-                }
-                temp.push(Double.parseDouble(a));//записываем в стек
-                i--;
-
-            } else if (IsOperator(str[i])) { //если символ -оператор
-                //берем числа из стека
-                double a = (double) temp.pop();
-                double b = (double) temp.pop();
-                //выполняем операции над ними
-                switch (str[i]) {
-                    case '+':
-                        rezult = b + a;
-                        break;
-                    case '-':
-                        rezult = b - a;
-                        break;
-                    case '*':
-                        rezult = b * a;
-                        break;
-                    case '/':
-                        rezult = b / a;
-                        break;
-                }
-                temp.push(rezult);//результат возвращаем в стек
-            }
+    public static boolean sNumber(String ch) {
+        if (ch == null || ch.isEmpty()) return false;
+        for (int i = 0; i < ch.length(); i++) {
+            if (!Character.isDigit(ch.charAt(i))) return false;
         }
-        return (double) temp.peek();//возвращаем результат всех вычислений
-    }
-
-    private static String GetExpression(String expresion) {
-        int x;
-        String postfix = "";
-        Stack st = new Stack();
-        char[] str = expresion.toCharArray();
-        for (int i = 0; i < str.length; i++) {
-            x = Priority(str[i]);//находим приоритет символа
-            if (x == -1) {//Если символ пробел- пропускаем
-                continue;
-            }
-            if (x == 0 || x == 3) {//Если символ цифра или равно
-                while (x == 0 && i < str.length) {//пока цифр
-                    postfix += str[i];//добавляем каждую цифру числа к нашей строке
-                    i++;
-                    if (i == str.length) {//если символ последний выходим
-                        break;
-                    }
-                    x = Priority(str[i]);
-                }
-                postfix += " ";//дописываем пробел
-                i--;
-                continue;
-            }
-            if (x == 1) { //если скобка открывается
-                st.push(str[i]);//добавляем к стек
-                continue;
-            }
-            if (x == 2) {//если скобка закрывается
-                char symbol = (char) st.pop();
-                while (symbol != '(' && !st.empty()) { //пока не окрывающияся скобка или не конец стека
-                    postfix += symbol + " ";//записываем операции в строку
-                    symbol = (char) st.pop();
-                }
-
-                continue;
-            }
-            if (x == 4 || x == 5) {//если  +-/*
-                while (!st.empty() && Priority((char) st.peek()) >= x) {//пока не конец стека и приоритет оператора в стеке больше оператора в строке
-                    postfix += (char) st.pop() + " ";//извлекаем из стека в строку
-                }
-                st.push(str[i]);//заносим новый оператор в строку
-            }
-        }
-        while (!st.empty()) {//забираем последнии символы из стека
-            postfix += (char) st.pop() + " ";
-        }
-        return postfix;//возвращаем выражение в постфиксной записи
+        return true;
     }
 }
--- Laba3/src/test/java/CalculatorTest.java
+++ Laba3/src/test/java/CalculatorTest.java
@@ -6,11 +6,44 @@ import static org.junit.Assert.*;
 
 public class CalculatorTest {
     @org.junit.Test
-    public void calculate() throws Exception {
-        Calculator test1 = new Calculator();
-        double expected = 31d;
-        double actual = test1.Calculate("5*3+2*(12-4)");
-        assertEquals("Test", expected, actual, 0.001);
+    public void TestAddition() throws Exception {
+
+        double actual = Calculator.Calculate("11+23");
+        assertEquals("Addition check", 34d, actual, 0.001);
     }
 
+    @org.junit.Test
+    public void TestSubtraction() throws Exception{
+
+        double actual= Calculator.Calculate("23-13");
+        assertEquals("Check for subtraction", 10d, actual, 0.001);
+    }
+
+    @org.junit.Test
+    public void TestSegmentation() throws Exception{
+
+        double actual=Calculator.Calculate("45/5");
+        assertEquals("Fission check", 9d, actual, 0.001);
+    }
+
+    @org.junit.Test
+    public void TestMultiplication() throws Exception{
+
+        double actual=Calculator.Calculate("45*5");
+        assertEquals("Multiplication check", 225d, actual, 0.001);
+    }
+
+    @org.junit.Test
+    public void TestBrackets() throws Exception{
+
+        double actual=Calculator.Calculate("(45*1)*(102-2)");
+        assertEquals("Checking parentheses", 4500d, actual, 0.001);
+    }
+
+    @org.junit.Test
+    public void TestNull() throws Exception{
+
+        double actual=Calculator.Calculate(null);
+        assertEquals("Return null", 0, actual, 0.001);
+    }
 }
