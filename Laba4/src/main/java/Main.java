b7653a035e20013d6ed17a0f1e8d1003cb74a772
i1ya97 <31565819+i1ya97@users.noreply.github.com>
2017-11-24 23:24:58 +0700

    added changes

--- Laba4/src/main/java/Main.java
+++ Laba4/src/main/java/Main.java
@@ -4,10 +4,14 @@ import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args)throws FileNotFoundException {
-        FileAnalizer analysis = new FileAnalizer("input.txt");
+        FileAnalizer analysis = new FileAnalizer("large-file.txt");
+        System.out.println("ОК");
         System.out.println("Количество букв: "+analysis.countLetters());
+        System.out.println("ОК");
         System.out.println("Количетво слов :"+analysis.countWords());
+        System.out.println("ОК");
         System.out.println("Количество строк :"+analysis.countLines());
+        System.out.println("ОК");
         System.out.println("Частотный анализ :"+analysis.countFrequencyCharacteristic());
     }
 }
